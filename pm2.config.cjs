35c0ab110e7bd9d6365e3db667e8b91c3f0b52d7
KirillDanProg <kirill2898v@gmail.com>
2023-06-08 21:42:10 +0300

    bag fixing process

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -7,8 +7,8 @@ module.exports = {
             script: 'src/main.js',
             env: {
                 NODE_ENV: 'production',
-                TELEGRAM_API_KEY: '6053708681:AAFIsQ2oQhNPpgne1QphDN5hU_LOl-8mEuc',
-                OPENAI_API_KEY: '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'
+                TELEGRAM_API_KEY: process.env.TELEGRAM_API_KEY,
+                OPENAI_API_KEY: process.env.OPENAI_API_KEY
             },
         },
     ],
